cb289ff8b460146b5101e56208fffb3a1484bf03
Omar Hamad <etatldr@gmail.com>
2023-07-21 18:38:16 +0300

    init test
    This is a test and will be rebased later and will add the following:
    - redis
    - SSL

---
+++ terraform/0-provider.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "domain_name" {
+  type    = string
+  default = "etahamad.space"
+}
+
+variable "hosted_zone_id" {
+  type    = string
+  default = "Z04772373961TRUOY0HU6"
+}
